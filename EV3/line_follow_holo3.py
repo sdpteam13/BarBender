cc9fc90125f9fbcbe1baf619413179a55dca3389
Tianyu Wang <1463026393@qq.com>
2019-02-05 16:53:57 +0000

    upload file on behalf of Zeke

---
+++ EV3/line_follow_holo3.py
@@ -0,0 +1,103 @@
+import ev3dev.ev3 as ev3
+import time
+from robot_holo import Robot
+
+# based on line_follow_holo2.py
+# more object-oriented
+
+# to use this just:
+
+# from line_follow_tank2 import LineFollower
+# lf = LineFollower()
+# lf.run()
+
+class LineFollower():
+    def __init__(self):
+        self.robot = Robot()
+        self.motortime = 1000
+        self.speed = 3
+        self.turning_direction = 1 #1 = left, 2 = right
+        self.flag = True
+
+    def forward(self):
+        for i in range(int(self.motortime / 100)):
+            if self.robot.way_blocked():
+                self.robot.stop()
+                time.sleep(0.2)
+                i = i - 1
+            else:
+                self.robot.straight_line_moving(self.speed * 100, duration = 120)
+
+    def left_turn(self, speed, t):
+        self.robot.rotate_left(speed * 100, duration = t)
+
+    def right_turn(self, speed, t):
+        self.robot.rotate_right(speed * 100, duration = t)
+
+    def turn(self):
+        # Turn for 0.1s and a bit more for smoothness
+        if (self.turning_direction == 1):
+            self.left_turn(self.speed, 100)
+        else:
+            self.right_turn(self.speed, 100)
+
+    def change_turn_direction(self):
+        if (self.turning_direction == 1):
+            self.turning_direction = 2
+        else:
+            self.turning_direction = 1
+
+    def find_line(self, iterations = 3):
+        # If first attempt, set iteration to 3 for minor changes on a straight line
+
+        # Turn one way first
+        for i in range(iterations):
+            while self.robot.way_blocked():
+                self.robot.stop()
+            if (self.robot.line_detected()):
+                return
+            self.turn()
+            time.sleep(0.1)
+
+        # Turn opposite direction, past original angle
+        self.change_turn_direction()
+
+        for i in range(iterations * 2):
+            while self.robot.way_blocked():
+                self.robot.stop()
+            if (self.robot.line_detected()):
+                return
+            self.turn()
+            time.sleep(0.1)
+
+        # Back to original angle
+        self.change_turn_direction()
+
+        for i in range(iterations):
+            while self.robot.way_blocked():
+                self.robot.stop()
+            if (self.robot.line_detected()):
+                return
+            self.turn()
+            time.sleep(0.1)
+
+        # Increase search space
+        self.find_line(iterations + 5)
+
+
+    def run(self):
+        while True:
+            try:
+                if (not self.robot.line_detected()):
+                    self.find_line()
+                self.forward()
+
+            except Exception as e:
+                print(e)
+                pass
+
+    def stop(self):
+        self.flag = False
+
+#lineFollower = LineFollower()
+#lineFollower.run()
