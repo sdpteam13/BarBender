75a78eab43ed36417f893b99ee768d3f35dfccca
Tianyu Wang <1463026393@qq.com>
2019-03-09 17:09:23 +0000

    add demo3 folder

---
+++ EV3/demo3/cup_dispenser_server/server.py
@@ -0,0 +1,112 @@
+print ("server starting...")
+
+import socketserver
+import ev3dev.ev3 as ev3
+import time
+
+# DC motor
+p = ev3.LegoPort('outB')
+assert p.connected
+p.mode = 'dc-motor'
+time.sleep(1)
+
+# motors and sensors
+lift_motor = ev3.LargeMotor('outD')
+grab_motor = ev3.DcMotor('outB')
+lift_sensor = ev3.TouchSensor('in1')
+grab_sensor = ev3.TouchSensor('in2')
+
+lift_motor.stop_action='brake'
+
+class cup_dispenser:
+
+    '''def __init__(self):
+        self.initialise()'''
+
+    # go to the initialisation spot
+    def initialise(self):
+        self.lift_down()
+        self.grab_reset()
+        self.grab_open()
+        self.lift_up()
+    
+    # calibrate, close the grabber
+    def grab_reset(self):
+        while not grab_sensor.is_pressed:
+            grab_motor.run_timed(duty_cycle_sp=-100, time_sp=100)
+        grab_motor.stop()
+
+    # lift goes down
+    def lift_down(self):
+        while not lift_sensor.is_pressed:
+            lift_motor.run_timed(speed_sp=450, time_sp=80)
+        lift_motor.stop()
+        time.sleep(0.2)
+
+    # open the grabber
+    def grab_open(self):
+        grab_motor.run_timed(duty_cycle_sp=100, time_sp=800)
+        time.sleep(1)
+        
+    # close the grabber but pose more force in order to grab the cup
+    def grab_close(self):
+        grab_motor.run_timed(duty_cycle_sp=-100, time_sp=1300)
+        time.sleep(1.5)
+        
+    # lift goes up
+    def lift_up(self):
+        lift_motor.run_timed(speed_sp=-450, time_sp=830)
+        time.sleep(1)
+
+    def cup_grab_fail(self):
+        grab_motor.run_timed(duty_cycle_sp=-100, time_sp=200)
+        time.sleep(.2)
+        return grab_sensor.is_pressed
+    
+    # grab an empty cup
+    def grab_and_down(self):
+        self.grab_close()
+        self.lift_down()
+        # grab to check if there is a cup grabbed
+        if self.cup_grab_fail():
+            #no cup, try again
+            self.grab_open()
+            self.lift_up()
+            self.grab_and_down()
+            return
+        
+        self.grab_open()
+        self.lift_up()
+
+class EchoRequestHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        # Echo the back to the client
+        data = self.request.recv(1024)
+        st = data.decode('utf8')
+        print(st)
+        cp = cup_dispenser()
+        cp.grab_and_down()
+        return
+
+if __name__ == '__main__':
+    import socket
+    import threading
+
+    address = ('0.0.0.0', 25565) # that'SSSsss a nice port you have there
+    server = socketserver.TCPServer(address, EchoRequestHandler)
+    ip, port = server.server_address # find out what port we were given
+    print ("server started!")
+    print ("ip: " + str(ip))
+    print ("port: " + str(port))
+
+    cp = cup_dispenser()
+    cp.initialise()
+    cp.grab_and_down()
+
+    t = threading.Thread(target=server.serve_forever)
+    t.setDaemon(True) # don't hang on exit
+    t.start()
+
+    while True:
+        time.sleep(1)
---
+++ EV3/demo3/line_follow_holo3.py
@@ -0,0 +1,168 @@
+import ev3dev.ev3 as ev3
+import time
+from environment import Environment
+
+# based on line_follow_holo2.py
+# more object-oriented
+
+# to use this just:
+
+# from line_follow_tank2 import LineFollower
+# lf = LineFollower()
+# lf.run()
+env = Environment()
+
+class LineFollower():
+    def __init__(self, robot, env):
+        self.robot = robot
+        self.env = env
+        self.motortime = 1000
+        self.speed = 2.5
+        self.turning_direction = 1 #1 = left, 2 = right
+        self.flag = True
+        self.offline = 0
+        self.left_adjust = 0
+        self.right_adjust = 0
+
+    #def forward(self):
+    #    # not used for now, remove?
+    #    for i in range(int(self.motortime / 100)):
+    #        if self.robot.way_blocked():
+    #            self.robot.stop()
+    #            time.sleep(0.2)
+    #            i = i - 1
+    #        else:
+    #            self.robot.straight_line_moving(self.speed * 100, duration = 120)
+
+    def left_turn(self, speed, t):
+        self.robot.rotate_left(speed * 100, duration = t)
+
+    def right_turn(self, speed, t):
+        self.robot.rotate_right(speed * 100, duration = t)
+
+    def turn(self, speed):
+        # Turn for 0.1s and a bit more for smoothness
+        if (self.turning_direction == 1):
+            self.left_turn(speed, 100)
+        else:
+            self.right_turn(speed, 100)
+
+    def change_turn_direction(self):
+        if (self.turning_direction == 1):
+            self.turning_direction = 2
+        else:
+            self.turning_direction = 1
+
+    def target_sensed(self, intersectionColor = env.corner_color):
+        return self.robot.line_detected() or self.robot.color_detected(intersectionColor)
+        #return self.robot.line_detected();
+
+    def find_line(self, iterations = 3, intersectionColor = env.corner_color):
+        # If first attempt, set iteration to 3 for minor changes on a straight line
+
+        # Turn one way first
+        for i in range(iterations):
+            while self.robot.way_blocked():
+                self.robot.stop()
+            if self.target_sensed(intersectionColor):
+                return
+            self.turn(self.speed)
+            time.sleep(0.1)
+
+        # Turn opposite direction, past original angle
+        self.change_turn_direction()
+
+        for i in range(iterations * 2):
+            while self.robot.way_blocked():
+                self.robot.stop()
+            if self.target_sensed(intersectionColor):
+                return
+            self.turn(self.speed)
+            time.sleep(0.1)
+
+        # Back to original angle
+        self.change_turn_direction()
+
+        for i in range(iterations):
+            while self.robot.way_blocked():
+                self.robot.stop()
+            if self.target_sensed(intersectionColor):
+                return
+            self.turn(self.speed)
+            time.sleep(0.1)
+
+        # Increase search space
+        self.find_line(iterations + 2)
+
+    def iteration(self):
+        if (self.robot.way_blocked()):
+            self.robot.stop()
+            return
+
+        detected_R = self.robot.line_detected_right()
+        detected_M = self.robot.line_detected_middle()
+        detected_L = self.robot.line_detected_left()
+
+        if (not (detected_R or detected_M or detected_L)):
+            self.offline = self.offline + 1
+            #print("unfind", self.offline)
+            self.find_line()
+
+        # else:
+        #     print(self.robot.gy.angle)
+        #     self.robot.steer_by_degree(degrees = -self.robot.gy.angle)
+        elif (detected_L):
+            self.left_adjust = self.left_adjust + 1
+            #print("left adjust", self.left_adjust)
+            #self.robot.rotate_left(80)
+            self.robot.steer_left(300)
+        elif (detected_R):
+            self.right_adjust = self.right_adjust + 1
+            #print("right adjust", self.right_adjust)
+            #self.robot.rotate_right(80)
+            self.robot.steer_right(300)
+        elif (detected_M):
+            self.robot.straight_line_moving()
+
+        else:
+            # shouldn't be triggered
+            pass
+        
+    def iteration_backwards(self, speed = 150):
+        if (self.robot.way_blocked()):
+            self.robot.stop()
+            return
+
+        detected_R = self.robot.line_detected_right()
+        detected_M = self.robot.line_detected_middle()
+        detected_L = self.robot.line_detected_left()
+
+        if (not (detected_R or detected_M or detected_L)):
+            self.offline = self.offline + 1
+            #print("unfind", self.offline)
+            self.find_line()
+
+        elif (detected_L):
+            self.left_adjust = self.left_adjust + 1
+            #print("left adjust", self.left_adjust)
+            self.robot.steer_left(-150)
+        elif (detected_R):
+            self.right_adjust = self.right_adjust + 1
+            #print("right adjust", self.right_adjust)
+            self.robot.steer_right(-150)
+        elif (detected_M):
+            self.robot.straight_line_moving_backwards(speed)
+        else:
+            # shouldn't be triggered
+            pass
+
+    def run(self):
+        while True:
+            try:
+                self.iteration()
+            except Exception as e:
+                print(e)
+                pass
+
+#lf = LineFollower()
+#lf.run()
