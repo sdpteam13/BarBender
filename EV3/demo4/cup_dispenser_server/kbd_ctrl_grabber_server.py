41d0d7c9d9a95fa5d999f5ffa4a0308970ff149e
伊藤程 <zhanshenlc@live.com>
2019-04-04 17:29:18 +0100

    New kbd test

--- EV3/demo4/cup_dispenser_server/kbd_ctrl_grabber_server.py
+++ EV3/demo4/cup_dispenser_server/kbd_ctrl_grabber_server.py
@@ -1,11 +1,14 @@
-from server import cup_dispenser
+from cup_server import cup_dispenser
 
 cp = cup_dispenser()
 
 print("Started")
 
 cp.initialise()
+cp.grab_open_more()
 
 while True:
     input()
+    cp.grab_close_more()
     cp.grab_and_down()
+    cp.grab_open_more()
